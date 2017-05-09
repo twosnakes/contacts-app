1150768f115a7679f5ae614b4a5f1557563a0133
Zoe Lembeck <zlembeck@gmail.com>
2017-05-08 21:34:51 -0500

    made restful and flash and redirect

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,42 @@
 class ContactsController < ApplicationController
-  def one_contact
-    @contact = Contact.first
-    render 'one_contact.html.erb'
+
+
+  def index
+    @contacts = Contact.all
   end
 
-  def many_contacts
-    @contacts = Contact.all
-    render 'many_contacts.html.erb'
+  def new
+
   end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+      )
+    @contact.save
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    contact = Contact.find(params[:id])
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+  end
+
+
+
+
 end
